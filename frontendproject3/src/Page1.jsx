73b12bbe8443436b7ca55688b204c7be4a16e2ff
ATMAKURIDHANUSH33102 <atmakuridhanush94@gmail.com>
2025-08-21 10:57:14 +0530

    first commit at 21-8-2025

---
+++ frontendproject3/src/Page1.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import'./css/Page1.css'
+import axios from 'axios';
+
+export default class Page1 extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      weather: null
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ city: e.target.value });
+  };
+
+  getWeather = () => {
+    const apiKey = '7ea2310649a55dd4075a464a8005fc2e'; // Replace with your OpenWeatherMap API key
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${apiKey}&units=metric`)
+      .then((res) => {
+        this.setState({ weather: res.data });
+      });
+  };
+  render() {
+    return (
+      <div >
+        <div className='cod'>
+        <h1>Weather App</h1>
+        <input type="text" placeholder="Enter city" onChange={this.handleChange} />
+        <button onClick={this.getWeather}><label >Get Weather</label></button>
+        </div>
+        <div className='cod'>
+
+        {this.state.weather&&(
+            <div>
+                <h2>{this.state.weather.name}</h2>
+                <p>Temperature:{this.state.weather.main.temp}c</p>
+                <p>{this.state.weather.weather[0].description}</p>
+            </div>
+        )}
+        </div>
+        
+      </div>
+    )
+  }
+}
